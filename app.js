e1974e7b59c02e416455d7a7ce4090714b242c0a
Shannon Wolff <sredline182@gmail.com>
2019-08-14 20:03:07 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('We are on home');
+    res.send('Hello World!');
   });
   
 module.exports = app;
